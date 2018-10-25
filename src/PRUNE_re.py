087f37ec5f9606119e5ee4719b6c3fa2ebd21e29
manish1596 <mksingh0296@gmail.com>
2018-10-25 21:03:33 +0530

    Added command line arguments and dumped final embeddings into the output file

--- src/PRUNE_re.py
+++ src/PRUNE_re.py
@@ -1,20 +1,45 @@
 # PRUNE Restructured
 
+import argparse
 import sys
 import numpy as np
 import tensorflow as tf
 from basic_prune import PRUNE, calc_pmi, full_loss
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Restructured PRUNE.")
+
+    parser.add_argument('--inputgraph', nargs='?',
+                        default='sample/graph.edgelist',
+                        help='Input graph path')
+
+    parser.add_argument('--output', nargs='?', default='graph.embeddings',
+                        help='Output node embeddings of the graph')
+
+    parser.add_argument('--dimension', type=int, default=128,
+                        help='Embedding dimension. Default is 128.')
+
+    parser.add_argument('--lamb', type=float, default=0.01,
+                        help='Parameter lambda in objective. Default is 0.01.')
+
+    parser.add_argument('--learning_rate', type=float, default=1e-4,
+                        help='Learning rate for Adam. Default is 1e-4.')
+
+    parser.add_argument('--epoch', type=int, default=50,
+                        help='Training epochs. Default is 50.')
+
+    args = parser.parse_args()
+
     # parameters
-    input_graph = sys.argv[1]
-    embedding_size = 100
+    #input_graph = sys.argv[1]
+    embedding_size = args.dimension
     hidden_size = 64
-    num_epochs = 100
-    lamb = 0.01
-    learning_rate = 0.01
+    num_epochs = args.epoch
+    lamb = args.lamb
+    learning_rate = args.learning_rate
     
-    graph = np.loadtxt(input_graph).astype(np.int32)
+    graph = np.loadtxt(args.inputgraph).astype(np.int32)
     nodeCount = graph.max() + 1
     out_degrees = np.zeros([nodeCount, 1])
     in_degrees = np.zeros([nodeCount, 1])
@@ -45,5 +70,8 @@ if __name__ == "__main__":
             outdeg: out_degrees[graph[:, 0]]
         }
         for i in range(num_epochs):
+            print("Epoch-->" + str(i+1) + " ...")
             sess.run(optimizer, feed_dict=feed_dict)
             print(sess.run(cost, feed_dict=feed_dict))
+        final_embeddings = sess.run(model.E.W)
+        np.savetxt(args.output, final_embeddings, delimiter = ",")
